ab2eb557d7b68b7183f159bea93e3fd23aedbedc
ebai101 <ebailey256@gmail.com>
2015-08-16 21:46:47 -0400

    corrected code, implemented full download system

--- assets/js/gate.js
+++ assets/js/gate.js
@@ -1,14 +1,50 @@
-SC.initialize({
-    client_id: = "068d7b70ba3ff663fc2f252c8533d291"
-    redirect_uri: = "/redirect.html"
-});
+window.gate = {};
 
-SC.connect(function(){
-    SC.get('/me/followings/23445206')
-            .then(function(r) {
-                if (r.errors != undefined) {
-                        SC.put('/me/followings/23445206');
-                }
-            });
+var SOUNDCLOUD_ID = "068d7b70ba3ff663fc2f252c8533d291";
+var DOWNLOAD_LINK = null;
+
+function locker() {
+    var g = window.gate;
+    if(g.soundcloud) {
+	//changes things
+        $("img.download").attr({
+	    src: "assets/buttons/dlbutton.png"
+	});
+        $("a.download").attr({
+            href: DOWNLOAD_LINK
+        });
+	$("img.soundcloud").attr({
+	   src: "assets/buttons/scdonebutton.png" 
+	});
+	$("a.soundcloud").attr({
+	    href: "#"
+	});
+    }
+}
+
+hello.on("auth.login", function(auth) {
+    if(auth.network == "soundcloud") {
+        console.log("Logging into soundcloud.");
+
+	//makes handshake
+        hello("soundcloud")
+        .api("soundcloud:/me/followings/23445206", "get", {client_id: SOUNDCLOUD_ID})
+        .then(function(r) {
+            window.gate.soundcloud = true;
+	    
+	    //follows if not following
+            if(r.errors != undefined) {
+                hello("soundcloud")
+                .api("soundcloud:/me/followings/23445206.json", "put",
+                     {client_id: SOUNDCLOUD_ID});
+            }
+
+	    //opens locker
+            locker();
+        });
     }
 });
+
+hello.init({
+    soundcloud: SOUNDCLOUD_ID,
+}, {redirect_uri: 'http://soupandreas.com/callback.html'});
