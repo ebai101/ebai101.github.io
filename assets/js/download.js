c96be289646aae7afcde60e5359b74593846f513
ebai101 <ebailey256@gmail.com>
2015-08-24 14:46:19 -0400

    timestamp fix

--- assets/js/download.js
+++ assets/js/download.js
@@ -1,4 +1,4 @@
-var insert_sql = "  INSERT INTO data.downloads VALUES (%[REMOTE_ADDR]||%(usrId), %(tstamp));"
+var insert_sql = "  INSERT INTO data.downloads VALUES (%[REMOTE_ADDR], %(tstamp));"
 
 $(document).ready(function () {
     $.rdbHostConfig({ 'userName' : 'p0000001604',
@@ -13,7 +13,7 @@ function download() {
     var usrId = Math.floor(Math.random()*1000);
     var time = $.now();
     $.postData( { q : insert_sql,
-		   namedParams : { 'usrId' : usrId, "tstamp" : time },
+		   namedParams : { 'tstamp' : time },
 		   callback : exit,
 		   errback : function (err) {
 		       console.log('sql error '+err[0]+err[1])
